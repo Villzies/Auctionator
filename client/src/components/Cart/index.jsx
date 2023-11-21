883d2f431f63a0d75ef6a3f3e0f37a8dc71b1111
Disastris <chavez.garcia.santi@gmail.com>
2023-11-20 19:42:11 -0600

    Added Global State under utils

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -5,7 +5,7 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-// import { useStoreContext } from '../../utils/GlobalState'; Don't forget to create GlobalState.jsx file in the utils folder
+import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css'; 
 
