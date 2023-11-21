81968c5da1da1a130b99ce44db33b1590f0f547e
Villzies <gemberling.stephan@att.net>
2023-11-20 20:42:00 -0600

    text update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Home from './pages/home.jsx';
 import Login from './pages/login.jsx';
 import Signup from './pages/Signup.jsx';
-import App from './App.jsx'
+import App from './app.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
