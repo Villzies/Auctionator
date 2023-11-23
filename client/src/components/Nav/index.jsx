0e020bbc9a01dbf32ee923ea318813af1f0ae248
Fancy Nancy <ramireznancy.686@gmail.com>
2023-11-22 21:11:42 -0700

    added nav bar options

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -3,36 +3,69 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
 
+    function handleLogout() {
+        Auth.logout();
+    }
+
     function showNavigation() {
         if (Auth.loggedIn()) {
             return (
-                <ul className="flex-row">
-                    <li className="mx-1">
-                        <Link to="/orderHistory">
-                            Order History
-                        </Link>
+                <div className="navbar">
+                    <ul className="flex-row">
+                        <li className="mx-1">
+                            <Link to="/">
+                                Home
+                            </Link>
                         </li>
                         <li className="mx-1">
-                            <a href='/' onClick={() => Auth.logout()}>
+                            <Link to="/about">
+                                About Us
+                            </Link>
+                        </li>
+                        <li className="mx-1">
+                            <Link to="/contact">
+                                Contact Us
+                            </Link>
+                        </li>
+                        <li className="mx-1">
+                            <Link to="/" onClick={handleLogout}>
                                 Logout
-                            </a>
+                            </Link>
                         </li>
                     </ul>
+                </div>
             );
-        }   else {
+        } else {
             return (
-                <ul className='flex-row'>
-                    <li className='mx-1'>
-                        <Link to="/signup">
-                            Signup
-                        </Link>
-                    </li>
-                    <li className='mx-1'>
-                        <Link to='/login'>
-                            Login
-                        </Link>
-                    </li>
-                </ul>
+                <div className="navbar">
+                    <ul className='flex-row'>
+                        <li className='mx-1'>
+                            <Link to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className='mx-1'>
+                            <Link to="/about">
+                                About Us
+                            </Link>
+                        </li>
+                        <li className='mx-1'>
+                            <Link to="/contact">
+                                Contact Us
+                            </Link>
+                        </li>
+                        <li className='mx-1'>
+                            <Link to="/signup">
+                                Signup
+                            </Link>
+                        </li>
+                        <li className='mx-1'>
+                            <Link to='/login'>
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             );
         }
     }
@@ -45,9 +78,7 @@ function Nav() {
                 </Link>
             </h1>
 
-            <nav>
-                {showNavigation()}
-            </nav>
+            {showNavigation()}
         </header>
     );
 }
