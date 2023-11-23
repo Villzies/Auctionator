be789888790ef49600ce06d8dedf495086ea5676
Villzies <gemberling.stephan@att.net>
2023-11-22 21:31:01 -0600

    Added card and form for auction to home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -5,10 +5,45 @@ const Home = () => {
   return (
     <div>
       <h2>Welcome to Auctionator!</h2>
-      <div className="auction-item-container">
-        <AuctionItem />
+  <div className="auction-item-container">
+      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
+<AuctionItem />
+<form>
+<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Bid Amount
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="$500"
+                  />
+                </div>
+              </div>
+            </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Bitches Be Bidding
+        </button>
       </div>
-    </div>
+      </form>
+    
+      </a>
+      </div>
+  </div>
   );
 };
 
