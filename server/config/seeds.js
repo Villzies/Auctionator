8be07c019c1c3665cb3b0d25ed2273653d1d1b33
Fancy Nancy <ramireznancy.686@gmail.com>
2023-11-27 08:35:54 -0700

    added images to db and displayed on page

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -30,18 +30,46 @@ db.once("open", async () => {
 
     const auctionItems = await AuctionItem.insertMany([
       {
-        name: "Item 1",
-        description: "Description for Item 1",
-        startingPrice: 50.0,
-        imageURL: "https://example.com/item1.jpg",
+        name: "Air Jordans",
+        description:
+          "Air Jordan is a line of basketball shoes produced by Nike, Inc. Related apparel and accessories are marketed under Jordan Brand. The silhouette of Michael Jordan served as inspiration to create the Jumpman logo.",
+        startingPrice: 25.0,
+        imageURL: "/images/Air-Jordans.avif",
         auctionEndDate: "2023-12-24",
         bidHistory: [],
       },
       {
-        name: "Item 2",
-        description: "Description for Item 2",
-        startingPrice: 75.0,
-        imageURL: "https://example.com/item2.jpg",
+        name: "Amazon Gift Card",
+        description: "$100 Amazon Gift Card",
+        startingPrice: 10.0,
+        imageURL: "/images/Amazon.png",
+        auctionEndDate: "2023-12-29",
+        bidHistory: [],
+      },
+      {
+        name: "Nintendo Switch",
+        description:
+          "The Nintendo Switch is a versatile gaming console that offers both portable and home gaming experiences. Its innovative design allows users to seamlessly switch between handheld mode and TV mode for a diverse gaming experience.",
+        startingPrice: 50.0,
+        imageURL: "/images/Nintendo-Switch.avif",
+        auctionEndDate: "2023-12-30",
+        bidHistory: [],
+      },
+      {
+        name: "Pokemon Card",
+        description:
+          "Pokemon cards are collectible cards featuring various Pokémon characters from the Pokémon universe. Each card typically showcases a specific Pokémon's artwork, statistics, abilities, and attacks. These cards are used in the Pokémon Trading Card Game, where players assemble decks to battle against each other using their Pokémon's strengths and strategic moves.",
+        startingPrice: 10.0,
+        imageURL: "/images/Pokemon-Card.avif",
+        auctionEndDate: "2024-01-04",
+        bidHistory: [],
+      },
+      {
+        name: "Retinol Cream",
+        description:
+          "Retinol cream is a skincare product containing a form of vitamin A called retinol. It's widely used for its anti-aging properties, known to reduce the appearance of fine lines, wrinkles, and improve skin texture.",
+        startingPrice: 5.0,
+        imageURL: "/images/Retinol-Cream.jpeg",
         auctionEndDate: "2023-12-29",
         bidHistory: [],
       },
